8b0a8ae7d7321974cd771d41ca84e1db49a2dc59
cy-fqyb <111214948+cy-fqyb@users.noreply.github.com>
2023-01-14 04:46:06 +0800

    ts-02

--- 贪吃蛇Demo/src/index.ts
+++ 贪吃蛇Demo/src/index.ts
@@ -1,1 +1,23 @@
-console.log(123);
+import './style/index.scss'
+// 定义一个食物类
+class Food {
+    // 定义一个属性用来保存食物的元素
+    element:HTMLElement;
+    constructor(){
+        // !的意思是说明该项属性不会为空
+        this.element = document.getElementById('food')!;
+    }
+    // 定义一个获取食物水平坐标的方法
+    get X(){
+        return this.element.offsetLeft
+    }
+    // 定义一个方法用来获取食物的y轴坐标
+    get Y(){
+        return this.element.offsetTop
+    }
+    // 定义一个修改食物位置的方法
+    changeFood(){
+        // 生成一个随机的位置
+        
+    }
+}
